df1d9a14eb29d34b313c65f0abd785159a1d212d
Zach Leach <zachleach@techoverco.com>
2019-02-21 21:21:11 -0700

    Added method for c_shifter and verified that it worked.

--- lib/modules/shifts.rb
+++ lib/modules/shifts.rb
@@ -7,4 +7,8 @@ module Shifts
   def b_shifter(key = @random_number, date = six_digit_date)
     @b_shift = b_key(key).to_i + b_offset(date)
   end
+
+  def c_shifter(key = @random_number, date = six_digit_date)
+    @c_shift = c_key(key).to_i + c_offset(date)
+  end
 end
