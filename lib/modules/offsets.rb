20810da1ece7270811d6bd3dd0a9ae895279af6d
Zach Leach <zachleach@techoverco.com>
2019-02-21 18:54:50 -0700

    Added the method six_digit_date and tested that it can be used in the master_offset method.

--- lib/modules/offsets.rb
+++ lib/modules/offsets.rb
@@ -1,8 +1,12 @@
 module Offsets
 
-  def master_offset(date = todays_date)
+  def master_offset(date = six_digit_date)
     master_offset = date.to_i * date.to_i
     master_offset = master_offset.to_s[-4..-1].split.join
+  end
+
+  def six_digit_date
+    two_digit_day + two_digit_month + two_digit_year
   end
 
   def todays_date
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -45,9 +45,12 @@ class EnigmaTest < Minitest::Test
     assert_equal 2, @enigma.two_digit_year.chars.count
   end
 
-  def test_enigma_can_create_todays_date_from_date_generator
-    skip
-    assert_equal "210219", @enigma.master_offset
+  def test_enigma_can_create_six_digit_date
+    assert_equal 6, @enigma.six_digit_date.chars.count
+  end
+
+  def test_enigma_can_create_default_date_from_date_generator
+    assert_equal 4, @enigma.master_offset.chars.count
   end
 
   def test_enigma_can_create_offsets
