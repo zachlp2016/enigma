ae5d1de50c20e02735338dd0b33435155f603015
Zach Leach <zachleach@techoverco.com>
2019-02-24 21:06:03 -0700

    One final comment to delete.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -7,12 +7,3 @@ new_file = File.open(ARGV[1], "w")
 new_file.write(encryption[:encryption])
 
 print "Created '#{ARGV[1]}' with the key #{enigma.random_number} and date #{enigma.six_digit_date}\n"
-
-
-
-
-# create a Runner file called encrypt.rb that takes two command line arguments.
-# The first is an existing file that contains a message to encrypt. The
-# second is a file where your program should write the encrypted message.
-# In addition to writing the encrypted message to the file, your program
-# should output to the screen the file it wrote to, the key and the date.
