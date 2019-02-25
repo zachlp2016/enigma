6842a5242be1109f5ebeac44f7ead477c8ba29b5
Zach Leach <zachleach@techoverco.com>
2019-02-24 18:45:35 -0700

    Created encrypt.rb file for the cli interface.

---
+++ encrypt.rb
@@ -0,0 +1,14 @@
+require './lib/enigma.rb'
+require 'pry'
+
+
+ARGV
+
+
+
+# Add a command line interface for encryption and decryption. You should 
+# create a Runner file called encrypt.rb that takes two command line arguments.
+# The first is an existing file that contains a message to encrypt. The
+# second is a file where your program should write the encrypted message.
+# In addition to writing the encrypted message to the file, your program
+# should output to the screen the file it wrote to, the key and the date.
